216770916aa1957b607cd6ef8f6c1ad3b51e0295
Ninodev14 <115539009+Ninodev14@users.noreply.github.com>
2024-03-15 15:57:15 +0100

    ad -18 alert

--- src/js/check.js
+++ src/js/check.js
@@ -1,12 +1,35 @@
-function verifierAge() {
+function etudcheck() {
 
-    var ageVerifie = document.cookie.replace(/(?:(?:^|.*;\s*)ageVerifie\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+    var etud = document.cookie.replace(/(?:(?:^|.*;\s*)etud\s*\=\s*([^;]*).*$)|^.*$/, "$1");
 
-    if (ageVerifie !== "oui") {
+    if (etud !== "oui") {
         var confirmation = confirm("Ce site internet a été créé dans le cadre d'un exercice de l'IUT Bordeaux Montaigne.");
         if (confirmation) {
-            document.cookie = "ageVerifie=oui; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/";
+            document.cookie = "etud=oui; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/";
         }
     }
 }
+
+function verifierAge() {
+    // Vérifier si le cookie d'âge a déjà été défini
+    var ageVerifie = document.cookie.replace(/(?:(?:^|.*;\s*)ageVerifie\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+    
+    if (ageVerifie !== "oui") {
+        var confirmation = confirm("Êtes-vous âgé de 18 ans ou plus ?");
+        if (confirmation) {
+            // Définir un cookie pour indiquer que l'âge a été vérifié
+            document.cookie = "ageVerifie=oui; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/";
+            alert("Bienvenue !");
+        } else {
+            alert("Désolé, vous devez avoir 18 ans ou plus pour accéder à ce contenu.");
+        }
+    }
+}
+
+// Appeler la fonction de vérification de l'âge
+verifierAge();
+
+
+// Appeler la fonction de vérification de l'âge
+etudcheck();
 verifierAge();
