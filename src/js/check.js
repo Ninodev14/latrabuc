2f2a0e9fe90329b49f840619c286552afc6c1134
Ninodev14 <115539009+Ninodev14@users.noreply.github.com>
2024-03-15 10:25:56 +0100

    add backgroubnd

--- src/js/check.js
+++ src/js/check.js
@@ -1,15 +1,12 @@
-// Fonction pour afficher la pop-up si l'âge n'a pas déjà été vérifié
 function verifierAge() {
-    // Vérifier si le cookie d'âge a déjà été défini
+
     var ageVerifie = document.cookie.replace(/(?:(?:^|.*;\s*)ageVerifie\s*\=\s*([^;]*).*$)|^.*$/, "$1");
 
     if (ageVerifie !== "oui") {
         var confirmation = confirm("Ce site internet a été créé dans le cadre d'un exercice de l'IUT Bordeaux Montaigne.");
         if (confirmation) {
-            // Définir un cookie pour indiquer que l'âge a été vérifié
             document.cookie = "ageVerifie=oui; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/";
         }
     }
 }
-// Appeler la fonction de vérification de l'âge
 verifierAge();
